841bd9ef45872d12ee3abfd7dc7f8a0505216377
kaghog <grace.kagho@ivt.baug.ethz.ch>
2023-04-25 14:32:18 +0200

    option to create drt vehicles within boundary conditions

--- switzerland/src/main/java/org/eqasim/switzerland/drt/utils/RunCreateDrtVehicles.java
+++ switzerland/src/main/java/org/eqasim/switzerland/drt/utils/RunCreateDrtVehicles.java
@@ -6,6 +6,7 @@ import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.ConfigGroup;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.population.io.PopulationReader;
@@ -13,15 +14,20 @@ import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.utils.gis.ShapeFileReader;
 import org.opengis.feature.simple.SimpleFeature;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Collection;
 
 public class RunCreateDrtVehicles {
     public static void main(String[] args) throws CommandLine.ConfigurationException, IOException {
         CommandLine cmd = new CommandLine.Builder(args) //
                 .requireOptions("networkFile", "outputpath", "fleetSize", "operationStartTime", "operationEndTime", "seats")
-                .allowOptions("populationFile", "name_suffix", "identifier", "boundary-shapefile", "random-seed")//
+                .allowOptions("populationFile", "name_suffix", "identifier", "boundary-shapefile", "random-seed", "vehicle_name")//
                 .build();
+
+        String vehicleName = cmd.getOption("vehicle_name").isPresent() ? cmd.getOption("vehicle_name").get() : "drt";
+
 
         String nameSuffix = cmd.getOption("name_suffix").isPresent() ? cmd.getOption("name_suffix").get() : "drt_vehicles";
         //identifier to specify type of generation if pop density, random or by hubs
@@ -29,7 +35,7 @@ public class RunCreateDrtVehicles {
         Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
 
         boolean isUseBoundary = cmd.getOption("boundary-shapefile").isPresent();
-        if (isUseBoundary) {
+        /*if (isUseBoundary) {
             String boundaryPolygon = cmd.getOption("boundary-shapefile").get();
             ShapeFileReader shapeFileReader = new ShapeFileReader();
             Collection<SimpleFeature> zurich_shp = shapeFileReader.readFileAndInitialize(boundaryPolygon);
@@ -37,7 +43,7 @@ public class RunCreateDrtVehicles {
             MultiPolygon zurich = (MultiPolygon) ((SimpleFeatureImpl) zurich_shp.toArray()[0]).getAttribute(0);
             //todo for random generation since pop density only uses links of population within pop areas
             //todo make generic still for all - hub, density, etc.
-        }
+        }*/
 
 
         //read in the input files
@@ -56,9 +62,18 @@ public class RunCreateDrtVehicles {
 
         String taxisFile = cmd.getOptionStrict("outputpath") + "/" + nameSuffix + "_" + numberofVehicles + "_" + seats + ".xml";
 
-        CreateDrtVehicles createDrtVehicles = new CreateDrtVehicles(numberofVehicles, operationStartTime, operationEndTime, seats); //todo adjust to give one with random seed if option random seed given
+        CreateDrtVehicles createDrtVehicles = new CreateDrtVehicles(network, numberofVehicles, operationStartTime, operationEndTime, seats); //todo adjust to give one with random seed if option random seed given
 
-        createDrtVehicles.createVehicles(network, population, taxisFile);
+        if (isUseBoundary) {
+            createDrtVehicles.setUseBoundary(true);
+            String boundaryPolygon = cmd.getOption("boundary-shapefile").get();
+            URL boundaryUrl = new File(boundaryPolygon).toURI().toURL();
+            createDrtVehicles.generateBoundary(boundaryUrl);
 
+        }
+
+        createDrtVehicles.createVehicles(population, taxisFile, vehicleName);
+
+        System.out.println("Finished!!!");
     }
 }
