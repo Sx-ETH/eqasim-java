accc7e62131f8a1253e22e583aa08dc07273fc70
fmartiescofet <f.martiescofet@gmail.com>
2023-05-27 18:35:57 +0200

    New plots

--- switzerland/Analysis_notebooks/utils.py
+++ switzerland/Analysis_notebooks/utils.py
@@ -103,3 +103,20 @@ def get_drt_legs(output_directory):
         drt_legs.append(pd.read_csv(iter_path + 'drt_legs_drt.csv', sep=';'))
     
     return drt_legs
+
+def hex_to_RGB(hex_str):
+    """ #FFFFFF -> [255,255,255]"""
+    #Pass 16 to the integer function for change of base
+    return [int(hex_str[i:i+2], 16) for i in range(1,6,2)]
+
+def get_color_gradient(c1, c2, n):
+    """
+    Given two hex colors, returns a color gradient
+    with n colors.
+    """
+    assert n > 1
+    c1_rgb = np.array(hex_to_RGB(c1))/255
+    c2_rgb = np.array(hex_to_RGB(c2))/255
+    mix_pcts = [x/(n-1) for x in range(n)]
+    rgb_colors = [((1-mix)*c1_rgb + (mix*c2_rgb)) for mix in mix_pcts]
+    return ["#" + "".join([format(int(round(val*255)), "02x") for val in item]) for item in rgb_colors]
