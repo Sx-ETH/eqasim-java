138eb7bd9e0951e1045dc9528e4408c70750e7ef
fmartiescofet <f.martiescofet@gmail.com>
2023-06-11 19:14:49 +0200

    Add creation of vehicles for fixed demand

--- switzerland/src/main/java/org/eqasim/switzerland/utils/GenerateFixedDRTDemand.java
+++ switzerland/src/main/java/org/eqasim/switzerland/utils/GenerateFixedDRTDemand.java
@@ -1,14 +1,20 @@
 package org.eqasim.switzerland.utils;
 
+import org.eqasim.core.components.drt.utils.CreateDrtVehicles;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.*;
+import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,16 +23,24 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class GenerateFixedDRTDemand {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, CommandLine.ConfigurationException {
+        CommandLine cmd = new CommandLine.Builder(args) //
+                .requireOptions("populationFile", "outputDirectory", "drtDemand")//
+                .requireOptions("networkFile", "tripsPerVehicle")
+                .allowOptions("boundaryShapefile")//
+                .build();
 
-        String populationFile = args[0];
-        String outputfile = args[1];
-        int drtDemand = Integer.parseInt(args[2]);
+        String populationFile = cmd.getOptionStrict("populationFile");
+        String outputDirectory = cmd.getOptionStrict("outputDirectory");
+        int drtDemand = Integer.parseInt(cmd.getOptionStrict("drtDemand"));
+
+        String networkFile = cmd.getOptionStrict("networkFile");
 
         Config config = ConfigUtils.createConfig();
         Scenario scenario = ScenarioUtils.createScenario(config);
-        PopulationReader popReader = new PopulationReader(scenario);
-        popReader.readFile(populationFile);
+        Network network = scenario.getNetwork();
+        new MatsimNetworkReader(network).readFile(networkFile);
+        new PopulationReader(scenario).readFile(populationFile);
 
         ScenarioUtils.loadScenario(scenario);
         Population populationData = scenario.getPopulation();
@@ -46,7 +60,6 @@ public class GenerateFixedDRTDemand {
                     if (leg.getMode().equals("car") && distance >= 250) {
                         candidateLegs.add(leg);
                     }
-                    System.out.println(distance);
                 }
             }
         }
@@ -59,9 +72,34 @@ public class GenerateFixedDRTDemand {
             leg.setMode("drt");
         }
 
+        String outputfile = outputDirectory + "/fixed_demand_" + drtDemand + "_population.xml.gz";
         new PopulationWriter(populationData).write(outputfile);
 
-        System.out.println("Finished");
+        System.out.println("Finished outputting population file");
+        System.out.println("Starting creating DRT vehicles");
+
+        String vehiclesOutputFile = outputDirectory + "/fixed_demand_" + drtDemand + "_drt_vehicles.xml.gz";
+        int tripsPerVehicle = Integer.parseInt(cmd.getOptionStrict("tripsPerVehicle"));
+
+        String vehicle_name = "drt";
+        int seats = 4;
+        double operationStartTime = 0;
+        double operationEndTime = 24 * 3600;
+
+        int numberOfVehicles = (int) Math.ceil(((double) drtDemand) / tripsPerVehicle);
+        System.out.println("Number of vehicles: " + numberOfVehicles);
+
+        CreateDrtVehicles createDrtVehicles = new CreateDrtVehicles(network, numberOfVehicles, operationStartTime, operationEndTime, seats);
+
+        boolean isUseBoundary = cmd.hasOption("boundaryShapefile");
+        if (isUseBoundary) {
+            createDrtVehicles.setUseBoundary(true);
+            String boundaryPolygon = cmd.getOption("boundaryShapefile").get();
+            URL boundaryUrl = new File(boundaryPolygon).toURI().toURL();
+            createDrtVehicles.generateBoundary(boundaryUrl);
+        }
+
+        createDrtVehicles.createVehicles(populationData, vehiclesOutputFile, vehicle_name);
 
     }
 
