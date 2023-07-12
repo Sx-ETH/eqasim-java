67283d4ce0ef3841ce20e887e6c91b50ba4e89db
fmartiescofet <f.martiescofet@gmail.com>
2023-07-12 11:59:40 +0200

    Update RunSimulation.java

--- switzerland/src/main/java/org/eqasim/switzerland/RunSimulation.java
+++ switzerland/src/main/java/org/eqasim/switzerland/RunSimulation.java
@@ -9,6 +9,7 @@ import org.matsim.core.config.CommandLine.ConfigurationException;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
 
 public class RunSimulation {
@@ -21,6 +22,8 @@ public class RunSimulation {
         SwitzerlandConfigurator configurator = new SwitzerlandConfigurator();
         Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"), configurator.getConfigGroups());
         cmd.applyConfiguration(config);
+
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 
         Scenario scenario = ScenarioUtils.createScenario(config);
 
