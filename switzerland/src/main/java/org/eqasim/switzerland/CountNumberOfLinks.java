7a17c25bb5bc9ae0fc7fbfe33158714b7907e9af
fmartiescofet <f.martiescofet@gmail.com>
2023-09-29 15:14:39 +0200

    move to switzerland for standalone

--- switzerland/src/main/java/org/eqasim/switzerland/CountNumberOfLinks.java
+++ switzerland/src/main/java/org/eqasim/switzerland/CountNumberOfLinks.java
@@ -1,4 +1,4 @@
-package org.eqasim.core.tools;
+package org.eqasim.switzerland;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Network;
--- switzerland/src/main/java/org/eqasim/switzerland/CountNumberOfPeopleAndTrips.java
+++ switzerland/src/main/java/org/eqasim/switzerland/CountNumberOfPeopleAndTrips.java
@@ -1,4 +1,4 @@
-package org.eqasim.core.tools;
+package org.eqasim.switzerland;
 
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
